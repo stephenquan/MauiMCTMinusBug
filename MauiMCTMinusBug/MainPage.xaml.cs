443c3a861e342bef7c3e334d4c573bb2b0b65e67
Stephen Quan <squan@esri.com>
2025-06-16 11:15:34 +1000

    Initial check in

---
+++ MauiMCTMinusBug/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace MauiMCTMinusBug;
+
+public partial class MainPage : ContentPage
+{
+	CultureInfo ar_AR = new CultureInfo("ar-AR");
+	CultureInfo en_US = new CultureInfo("en-US");
+
+	public CultureInfo CurrentCulture
+	{
+		get => CultureInfo.CurrentCulture;
+		set
+		{
+			CultureInfo.CurrentCulture = value;
+			OnPropertyChanged(nameof(CurrentCulture));
+		}
+	}
+
+	public MainPage()
+	{
+		CurrentCulture = en_US;
+		BindingContext = this;
+		InitializeComponent();
+	}
+
+	void OnChangeCulture(object sender, EventArgs e)
+	{
+		CurrentCulture = (CurrentCulture.Name != ar_AR.Name) ? ar_AR : en_US;
+	}
+}
---
+++ MauiMCTMinusBug/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiMCTMinusBug;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
